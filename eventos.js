58c6d983157f5ea2159faf07043cb995e4d171de
Valeloyarte28 <valeloyarte@gmail.com>
2024-08-17 21:18:46 -0300

    initial commit

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 document.getElementById('miDiv').addEventListener('click', function() {
     alert('¡Hola! Soy el div');
 });
+
+document.querySelector('button').addEventListener('click', function(event) {
+    event.stopPropagation();
+    alert('¡Hola!');
+});
